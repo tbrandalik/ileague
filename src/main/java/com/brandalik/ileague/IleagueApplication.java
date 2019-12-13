e6da1fce7ced3fc104dbb8894858817dec7d2071
Tomas Brandalik <tbrandalik@gmail.com>
2019-12-13 17:06:30 +0100

    Initial commit

---
+++ src/main/java/com/brandalik/ileague/IleagueApplication.java
@@ -0,0 +1,13 @@
+package com.brandalik.ileague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IleagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IleagueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brandalik/ileague/IleagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandalik.ileague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IleagueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
